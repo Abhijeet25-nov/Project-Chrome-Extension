cf5667aece9ae811ce6772fa8c29d731773a58de
Abhijeet <absaxena2004@gmail.com>
2026-08-25 21:51:36 +0530

    Updating files

--- backend/reminder/urls.py
+++ backend/reminder/urls.py
@@ -3,6 +3,12 @@ from . import views
 
 urlpatterns=[
     path("add-reminder/",views.add_reminder,name="add_reminder"),
-    path("delete-remimder/<int:id>/",views.delete_reminder,name="delete_reminder")
+    path("delete-remimder/<int:id>/",views.delete_reminder,name="delete_reminder"),
+    path("api/reminders/", views.api_reminders, name="api_reminders"),
+    
+    path("api/reminders/<int:id>/", 
+         views.api_delete_reminder, 
+         name="api_delete_reminder"
+    ),
 ]
 
--- backend/reminder/views.py
+++ backend/reminder/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from .models import Reminder
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt 
+
 # Create your views here.
 
 def add_reminder(request):
@@ -12,6 +15,7 @@ def add_reminder(request):
         
         reminder=Reminder.objects.create(
             title=title,
+            
             date=date,
             time=time
         )
@@ -34,3 +38,60 @@ def delete_reminder(request, id):
         reminder.delete()
 
     return redirect("home")
+
+
+#----------------API---------------------
+@csrf_exempt
+def api_reminders(request):
+    
+    if request.method=="GET":
+    
+        reminders=Reminder.objects.all()
+        
+        data=[]
+        
+        for reminder in reminders:
+            data.append({
+                "id": reminder.id,
+                "title": reminder.title,
+                "date": str(reminder.date),
+                "time": str(reminder.time),
+            })
+            
+        return JsonResponse(data,safe=False)
+    
+    elif request.method == "POST":
+
+        title = request.POST.get("title")
+        date = request.POST.get("date")
+        time = request.POST.get("time")
+
+        reminder = Reminder.objects.create(
+            title=title,
+            date=date,
+            time=time
+        )
+
+        return JsonResponse({
+            "message": "Reminder created successfully",
+            "id": reminder.id,
+            "title": reminder.title,
+            "date": str(reminder.date),
+            "time": str(reminder.time)
+        })
+        
+@csrf_exempt
+def api_delete_reminder(request,id):
+    
+    if request.method=="DELETE":
+        reminder = Reminder.objects.get(id=id)
+        reminder.delete()
+        
+        return JsonResponse({
+            "message": "Reminder Deleted successfully"
+        })
+    
+    return JsonResponse({
+        "error" : "Only DELETE method is allowed"
+    })    
+            
